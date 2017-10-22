2df49601f16cbfb5a8c5a14a600973daa4e411bb
branflakes2 <brianw5@umbc.edu>
2017-10-22 14:33:58 -0400

    fixing aes

--- lab2/shift_row.vhd
+++ lab2/shift_row.vhd
@@ -5,47 +5,47 @@ use IEEE.std_logic_1164.all;
 entity shift_row is
 
 port(
-    input   :   in  std_logic_vector(127 downto 0);
-    output  :   out std_logic_vector(127 downto 0)
+    input   :   in  std_logic_vector(0 to 127);
+    output  :   out std_logic_vector(0 to 127)
 );
 end shift_row;
 
 architecture behavior of shift_row is 
 
     
-    alias a00   :   std_logic_vector(7 downto 0) is input(7 downto 0);
-    alias a10   :   std_logic_vector(7 downto 0) is input(15 downto 8);
-    alias a20   :   std_logic_vector(7 downto 0) is input(23 downto 16);
-    alias a30   :   std_logic_vector(7 downto 0) is input(31 downto 24);
-    alias a01   :   std_logic_vector(7 downto 0) is input(39 downto 32);
-    alias a11   :   std_logic_vector(7 downto 0) is input(47 downto 40);
-    alias a21   :   std_logic_vector(7 downto 0) is input(55 downto 48);
-    alias a31   :   std_logic_vector(7 downto 0) is input(63 downto 56);
-    alias a02   :   std_logic_vector(7 downto 0) is input(71 downto 64);
-    alias a12   :   std_logic_vector(7 downto 0) is input(79 downto 72);
-    alias a22   :   std_logic_vector(7 downto 0) is input(87 downto 80);
-    alias a32   :   std_logic_vector(7 downto 0) is input(95 downto 88);
-    alias a03   :   std_logic_vector(7 downto 0) is input(103 downto 96);
-    alias a13   :   std_logic_vector(7 downto 0) is input(111 downto 104);
-    alias a23   :   std_logic_vector(7 downto 0) is input(119 downto 112);
-    alias a33   :   std_logic_vector(7 downto 0) is input(127 downto 120);
+    alias a00   :   std_logic_vector(0 to 7) is input(0 to 7);
+    alias a10   :   std_logic_vector(0 to 7) is input(8 to 15);
+    alias a20   :   std_logic_vector(0 to 7) is input(16 to 23);
+    alias a30   :   std_logic_vector(0 to 7) is input(24 to 31);
+    alias a01   :   std_logic_vector(0 to 7) is input(32 to 39);
+    alias a11   :   std_logic_vector(0 to 7) is input(40 to 47);
+    alias a21   :   std_logic_vector(0 to 7) is input(48 to 55);
+    alias a31   :   std_logic_vector(0 to 7) is input(56 to 63);
+    alias a02   :   std_logic_vector(0 to 7) is input(64 to 71);
+    alias a12   :   std_logic_vector(0 to 7) is input(72 to 79);
+    alias a22   :   std_logic_vector(0 to 7) is input(80 to 87);
+    alias a32   :   std_logic_vector(0 to 7) is input(88 to 95);
+    alias a03   :   std_logic_vector(0 to 7) is input(96 to 103);
+    alias a13   :   std_logic_vector(0 to 7) is input(104 to 111);
+    alias a23   :   std_logic_vector(0 to 7) is input(112 to 119);
+    alias a33   :   std_logic_vector(0 to 7) is input(120 to 127);
 
-    alias o00   :   std_logic_vector(7 downto 0) is output(7 downto 0);
-    alias o10   :   std_logic_vector(7 downto 0) is output(15 downto 8);
-    alias o20   :   std_logic_vector(7 downto 0) is output(23 downto 16);
-    alias o30   :   std_logic_vector(7 downto 0) is output(31 downto 24);
-    alias o01   :   std_logic_vector(7 downto 0) is output(39 downto 32);
-    alias o11   :   std_logic_vector(7 downto 0) is output(47 downto 40);
-    alias o21   :   std_logic_vector(7 downto 0) is output(55 downto 48);
-    alias o31   :   std_logic_vector(7 downto 0) is output(63 downto 56);
-    alias o02   :   std_logic_vector(7 downto 0) is output(71 downto 64);
-    alias o12   :   std_logic_vector(7 downto 0) is output(79 downto 72);
-    alias o22   :   std_logic_vector(7 downto 0) is output(87 downto 80);
-    alias o32   :   std_logic_vector(7 downto 0) is output(95 downto 88);
-    alias o03   :   std_logic_vector(7 downto 0) is output(103 downto 96);
-    alias o13   :   std_logic_vector(7 downto 0) is output(111 downto 104);
-    alias o23   :   std_logic_vector(7 downto 0) is output(119 downto 112);
-    alias o33   :   std_logic_vector(7 downto 0) is output(127 downto 120);
+    alias o00   :   std_logic_vector(0 to 7) is output(0 to 7);
+    alias o10   :   std_logic_vector(0 to 7) is output(8 to 15);
+    alias o20   :   std_logic_vector(0 to 7) is output(16 to 23);
+    alias o30   :   std_logic_vector(0 to 7) is output(24 to 31);
+    alias o01   :   std_logic_vector(0 to 7) is output(32 to 39);
+    alias o11   :   std_logic_vector(0 to 7) is output(40 to 47);
+    alias o21   :   std_logic_vector(0 to 7) is output(48 to 55);
+    alias o31   :   std_logic_vector(0 to 7) is output(56 to 63);
+    alias o02   :   std_logic_vector(0 to 7) is output(64 to 71);
+    alias o12   :   std_logic_vector(0 to 7) is output(72 to 79);
+    alias o22   :   std_logic_vector(0 to 7) is output(80 to 87);
+    alias o32   :   std_logic_vector(0 to 7) is output(88 to 95);
+    alias o03   :   std_logic_vector(0 to 7) is output(96 to 103);
+    alias o13   :   std_logic_vector(0 to 7) is output(104 to 111);
+    alias o23   :   std_logic_vector(0 to 7) is output(112 to 119);
+    alias o33   :   std_logic_vector(0 to 7) is output(120 to 127);
 
 begin
 
